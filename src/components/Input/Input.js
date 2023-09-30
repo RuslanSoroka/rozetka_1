8f7d3acb76128dbad8b4e9b4738af1864d0207e4
Ruslan <rusik.sorok1337@gmail.com>
2023-09-30 15:32:44 +0300

    project-starts

---
+++ src/components/Input/Input.js
@@ -0,0 +1,5 @@
+import "./Input.css";
+
+const Input = () => {};
+
+export default Input;
---
+++ src/containers/Login/Login.js
@@ -0,0 +1,7 @@
+import "./Login.css";
+import Card from "../../components/Card/Card";
+import Input from "../../components/Input/Input";
+
+const Login = () => {};
+
+export default Login;
