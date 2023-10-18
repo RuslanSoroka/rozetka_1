2eeabd4d58fe3b6998c9bfb0088e1aa3ac859f41
Ruslan <rusik.sorok1337@gmail.com>
2023-10-18 16:22:50 +0300

    finished page(productId)

--- src/containers/Product/Product.js
+++ src/containers/Product/Product.js
@@ -1,17 +1,45 @@
 import "./Product.css";
 import Header from "../../components/Header/Header";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { PRODUCTS_API } from "../../constants";
+import IdProduct from "../../components/IdProduct/IdProduct";
+import { BsPatchCheck, BsPatchMinus, BsArrowLeft } from "react-icons/bs";
 
 const Product = () => {
     const { productId } = useParams();
+    const [selectedProduct, setSelectedProduct] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getProduct();
+    }, []);
+
+    const getProduct = async () => {
+        const response = await fetch(
+            `${PRODUCTS_API}/productsPreview/${productId}`
+        );
+        const data = await response.json();
+        setSelectedProduct([data]);
+    };
+
     return (
         <>
             <Header />
             <div className="main">
                 <div className="container">
-                    <p style={{ color: "red", fontSize: "30px" }}>
-                        Chousen product is: {productId}
-                    </p>
+                    <IdProduct
+                        className="product"
+                        data={selectedProduct}
+                        done={<BsPatchCheck className="bs-path-check" />}
+                        undone={<BsPatchMinus className="bs-path-minus" />}
+                        backButton={
+                            <BsArrowLeft
+                                className="bs-arrow-left"
+                                onClick={() => navigate(-1)}
+                            />
+                        }
+                    />
                 </div>
             </div>
         </>
